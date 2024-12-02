aaa9fa55d22e89ca5f48544f284375aba2e865f4
Vladimir Milyukov <vova.milyukov.94@bk.ru>
2024-12-02 13:57:08 +0300

    ffix

--- src/test/java/screens/OnboardingScreen/Onboarding.java
+++ src/test/java/screens/OnboardingScreen/Onboarding.java
@@ -2,6 +2,7 @@ package screens.OnboardingScreen;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static io.appium.java_client.AppiumBy.id;
@@ -9,13 +10,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Onboarding {
     private final SelenideElement textElement = $(id("org.wikipedia.alpha:id/primaryTextView"));
-    private final SelenideElement continueButton = $(id("org.wikipedia.alpha:id/continueButton"));
-    private final SelenideElement getStartButton = $(id("org.wikipedia.alpha:id/getStartButton"));
+    private final SelenideElement continueButton = $(id("org.wikipedia.alpha:id/fragment_onboarding_forward_button"));
+    private final SelenideElement getStartButton = $(id("org.wikipedia.alpha:id/fragment_onboarding_done_button"));
     private final SelenideElement searchContainer = $(id("org.wikipedia.alpha:id/search_container"));
 
     public void verifyText(String expectedText) {
-        textElement.shouldBe(visible);
-        assertThat(textElement.getText()).isEqualTo(expectedText);
+        textElement.shouldHave(text(expectedText));
     }
 
     public void clickContinue() {
